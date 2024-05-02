adf58774e2199249df608ee85fa34f7544cb9c06
radya100 <radya100@gmail.com>
2024-05-02 17:37:39 +0300

    feat: триггер и калк таблица для ПЧ

--- calc_create.sql
+++ calc_create.sql
@@ -20,3 +20,24 @@ create table calc.ci
 partition by toYYYYMM(dt_stream)
 order by key_source_hash;
 
+create materialized view integration.mv_to_calc_ci_from_loyalty_chequeitem_cur to calc.ci as
+select
+    cityHash64(ci_id, 'partner_id', 'chequeitem') as key_source_hash
+    , cityHash64('partner_id') as instance_hash
+    , cityHash64(article_id, 'partner_id') as article_hash
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.price'), 2) as price
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.quantity'), 2) as quantity
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.summdisc'), 2) as summdisc
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.summ'), 2) as summ
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.discount'), 2) as discount
+    , op = 'd' as is_del
+    , toInt64OrZero(JSON_VALUE(ba, '$.cheque_id')) as ch_id
+    , toInt64OrZero(JSON_VALUE((op = 'd' ? before : after as ba), '$.chequeitem_id')) as ci_id
+    , toInt32OrZero(JSON_VALUE(ba, '$.article_id')) as article_id
+    , ts_ms
+from integration.loyalty_chequeitem_cur;
+
+select * from calc.ci;
+
+select * from stage.loyalty_chequeitem_cur;
+
