fb73f6dfc420aca57bc757a088e07ee59daaf79a
radya100 <radya100@gmail.com>
2024-05-13 16:56:13 +0300

    feat: исправил calc.ci - добавил вычисляемый инстанс и соурс

--- calc_create.sql
+++ calc_create.sql
@@ -1,9 +1,11 @@
 create database if not exists calc;
 
+drop table if exists calc.ci;
 create table calc.ci
 (
-    key_source_hash UInt64
-    , instance_hash UInt64
+    key_instance_source_hash UInt64
+    , instance_id LowCardinality(String)
+    , source_id LowCardinality(String)
     , article_hash UInt64
     , price Decimal32(2)
     , quantity Decimal32(3)
@@ -18,13 +20,16 @@ create table calc.ci
     , dt_stream DateTime materialized parseDateTimeBestEffortOrZero(toString(ts_ms), 'UTC')
 ) engine = ReplacingMergeTree(ts_ms)
 partition by toYYYYMM(dt_stream)
-order by key_source_hash;
+order by key_instance_source_hash;
 
+
+drop table if exists integration.mv_to_calc_ci_from_loyalty_chequeitem_cur;
 create materialized view integration.mv_to_calc_ci_from_loyalty_chequeitem_cur to calc.ci as
 select
-    cityHash64(ci_id, 'partner_id', 'chequeitem') as key_source_hash
-    , cityHash64('partner_id') as instance_hash
-    , cityHash64(article_id, 'partner_id') as article_hash
+    cityHash64(ci_id, instance_id, source_id) as key_source_instance_hash
+    , JSON_VALUE(source, '$.name') as instance_id
+    , concat(lower(JSON_VALUE(source, '$.schema')), '.', lower(JSON_VALUE(source, '$.table'))) as source_id
+    , cityHash64(article_id, instance_id) as article_hash
     , toDecimal32OrZero(JSON_VALUE(ba, '$.price'), 2) as price
     , toDecimal32OrZero(JSON_VALUE(ba, '$.quantity'), 2) as quantity
     , toDecimal32OrZero(JSON_VALUE(ba, '$.summdisc'), 2) as summdisc
@@ -40,4 +45,22 @@ from integration.loyalty_chequeitem_cur;
 select * from calc.ci;
 
 select * from stage.loyalty_chequeitem_cur;
+
+insert into calc.ci
+select
+    cityHash64(ci_id, instance_id, source_id) as key_source_instance_hash --!
+    , JSON_VALUE(source, '$.name') as instance_id --!
+    , concat(lower(JSON_VALUE(source, '$.schema')), '.', lower(JSON_VALUE(source, '$.table'))) as source_id --!
+    , cityHash64(article_id, instance_id) as article_hash
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.price'), 2) as price
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.quantity'), 2) as quantity
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.summdisc'), 2) as summdisc
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.summ'), 2) as summ
+    , toDecimal32OrZero(JSON_VALUE(ba, '$.discount'), 2) as discount
+    , op = 'd' as is_del
+    , toInt64OrZero(JSON_VALUE(ba, '$.cheque_id')) as ch_id
+    , toInt64OrZero(JSON_VALUE((op = 'd' ? before : after as ba), '$.chequeitem_id')) as ci_id
+    , toInt32OrZero(JSON_VALUE(ba, '$.article_id')) as article_id
+    , ts_ms
+from stage.loyalty_chequeitem_cur;
 
