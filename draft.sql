a2cd4892b23bef33dd5de11051fbbf715202ab69
radya100 <radya100@gmail.com>
2024-05-20 16:41:47 +0300

    feat: вроде даже работает

--- draft.sql
+++ draft.sql
@@ -36,6 +36,7 @@ create or replace table draft.null_ea
 create or replace table draft.stage
 (
     key_id UInt64
+    , source LowCardinality(String)
     , ch_id UInt64
     , ci_id UInt64
     , bh_id UInt64
@@ -49,9 +50,9 @@ create or replace table draft.stage
     , eh_value Int32
     , ei_value Int32
     , is_del UInt8
-    , dt_create DateTime materialized now()
+    , dt_create DateTime64 materialized now64()
 ) engine = ReplacingMergeTree()
-order by key_id;
+order by (key_id, is_del);
 
 create or replace table draft.keys
 (
@@ -64,17 +65,18 @@ create or replace table draft.keys
 drop table draft.mv_to_stage_from_ci;
 create materialized view draft.mv_to_stage_from_ci to draft.stage as
 select
-    cityHash64(ci_id, 'ci', is_del) as key_id
+    cityHash64(ci_id, 'ci') as key_id
     , ci_id
     , ch_id
     , is_del
     , art_id
+    , 'ci' as source
 from draft.null_ci;
 
 drop table draft.mv_to_keys_from_ci;
 create materialized view draft.mv_to_keys_from_ci to draft.keys as
 select
-    (arrayJoin(is_del = 1 ? [(h1, h2)] : [(cityHash64(key, 'ci', is_del) as h1, cityHash64(rel_key, 'ch', is_del) as h2), (h2, h1)]) as tup).1 as key_id
+    (arrayJoin(is_del = 1 ? [(h1, h2)] : [(cityHash64(key, 'ci') as h1, cityHash64(rel_key, 'ch') as h2), (h2, h1)]) as tup).1 as key_id
     , tup.2 as related_key_id
 from
 (
@@ -89,29 +91,31 @@ from
 drop table draft.mv_to_stage_from_ch;
 create materialized view draft.mv_to_stage_from_ch to draft.stage as
 select
-    cityHash64(ch_id, 'ch', is_del) as key_id
+    cityHash64(ch_id, 'ch') as key_id
     , ch_id
     , is_del
     , dt
+    , 'ch' as source
 from draft.null_ch;
 
 drop table draft.mv_to_keys_from_ch;
 create materialized view draft.mv_to_keys_from_ch to draft.keys as
 select
-    cityHash64(ch_id, 'ch', is_del) as key_id
-    , cityHash64(ch_id, 'ch', is_del) as related_key_id
+    cityHash64(ch_id, 'ch') as key_id
+    , cityHash64(ch_id, 'ch') as related_key_id
 from draft.null_ch;
 
 --=== BI ====
 drop table draft.mv_to_stage_from_bi;
 create materialized view draft.mv_to_stage_from_bi to draft.stage as
 select
-    cityHash64(bo_id, 'bi', is_del) as key_id
+    cityHash64(bo_id, 'bi') as key_id
     , ch_id
     , ci_id
     , bo_id as bi_id
     , is_del
     , value as bi_value
+    , 'bi' as source
 from draft.null_bo
 where is_del = 1 or (is_del = 0 and ci_id <> 0);
 
@@ -120,14 +124,16 @@ where is_del = 1 or (is_del = 0 and ci_id <> 0);
 drop table draft.mv_to_stage_from_bh;
 create materialized view draft.mv_to_stage_from_bh to draft.stage as
 select
-    cityHash64(bo_id, 'bh', is_del) as key_id
+    cityHash64(bo_id, 'bh') as key_id
     , ch_id
     , bo_id as bh_id
     , is_del
     , value as bh_value
+    , 'bh' as source
 from draft.null_bo
 where is_del = 1 or (is_del = 0 and ci_id = 0);
 
+drop table draft.mv_to_keys_from_bo;
 create materialized view draft.mv_to_keys_from_bo to draft.keys as
 select
     (arrayJoin([
@@ -136,9 +142,9 @@ select
                 (
                     multiIf
                     (
-                        is_del = 0 and ci_id = 0 , ((bo_id, 'bh', is_del), (ch_id, 'bh', is_del))
-                        , is_del = 0 and ci_id <> 0, ((bo_id, 'bi', is_del), (ci_id, 'bi', is_del))
-                        , ((bo_id, 'bi', is_del), (bo_id, 'bh', is_del))
+                        is_del = 0 and ci_id = 0 , ((bo_id, 'bh'), (ch_id, 'ch'))
+                        , is_del = 0 and ci_id <> 0, ((bo_id, 'bi'), (ci_id, 'ci'))
+                        , ((bo_id, 'ci'), (bo_id, 'ch'))
                     ) as key
                 ).1
             )
@@ -146,23 +152,25 @@ select
         ), (cityHash64(key.2), cityHash64(key.1))
     ]) as q).1 as key_id
     , q.2 as related_key_id
--- from (select 1 as bo_id, 0 as is_del, 10 as ci_id, 11 as ch_id union all select 2 as bo_id, 1 as is_del, 0 as ci_id, 0 as ch_id);
 from draft.null_bo;
+-- from (select 1 as bo_id, 0 as is_del, 10 as ci_id, 11 as ch_id union all select 2 as bo_id, 1 as is_del, 0 as ci_id, 0 as ch_id);
 
 
 --=== EI ====
 drop table draft.mv_to_stage_from_ei;
 create materialized view draft.mv_to_stage_from_ei to draft.stage as
 select
-    cityHash64(ea_id, 'ei', is_del) as key_id
+    cityHash64(ea_id, 'ei') as key_id
     , ch_id
     , ci_id
     , ea_id as ei_id
     , is_del
     , ea_val as ei_value
+    , 'ei' as source
 from draft.null_ea
 where is_del = 1 or (is_del = 0 and ci_id <> 0) ;
 
+drop table draft.mv_to_keys_from_ea;
 create materialized view draft.mv_to_keys_from_ea to draft.keys as
 select
     (arrayJoin([
@@ -171,9 +179,9 @@ select
                 (
                     multiIf
                     (
-                        is_del = 0 and ci_id = 0 , ((ea_id, 'eh', is_del), (ch_id, 'eh', is_del))
-                        , is_del = 0 and ci_id <> 0, ((ea_id, 'ei', is_del), (ci_id, 'ei', is_del))
-                        , ((ea_id, 'ei', is_del), (ea_id, 'eh', is_del))
+                        is_del = 0 and ci_id = 0 , ((ea_id, 'eh'), (ch_id, 'ch'))
+                        , is_del = 0 and ci_id <> 0, ((ea_id, 'ei'), (ci_id, 'ci'))
+                        , ((ea_id, 'ei'), (ea_id, 'eh'))
                     ) as key
                 ).1
             )
@@ -188,31 +196,79 @@ from draft.null_ea;
 drop table draft.mv_to_stage_from_eh;
 create materialized view draft.mv_to_stage_from_eh to draft.stage as
 select
-    cityHash64(ea_id, 'eh', is_del) as key_id
+    cityHash64(ea_id, 'eh') as key_id
     , ch_id
     , ea_id as eh_id
     , is_del
     , ea_val as eh_value
+    , 'eh' as source
 from draft.null_ea
 where is_del = 1 or (is_del = 0 and ci_id = 0);
 
 
 truncate table draft.stage;
 truncate table draft.keys;
-insert into draft.null_ci values (1, 1, 0, 1), (2, 1, 0, 1), (1, 0, 1, 0);
+
+insert into draft.null_ci values (1, 1, 0, 1);
+insert into draft.null_ci values (2, 1, 0, 1);
+insert into draft.null_ci values (1, 0, 1, 0);
+
 insert into draft.null_ch values (1, 0);
-insert into draft.null_bo values (11, 1, 1, 1000, 0), (10, 2, 1, 100, 0), (11, 0, 0, 0, 1);
+
+insert into draft.null_bo values (11, 1, 1, 1000, 0);
+insert into draft.null_bo values (10, 2, 1, 100, 0);
+insert into draft.null_bo values (11, 0, 0, 0, 1);
 insert into draft.null_bo values (13, 0, 1, 120, 0);
+
 insert into draft.null_ea values (110, 1, 1, 1000, 0);
-insert into draft.null_ea values (111, 0, 1, 222, 0), (111, 0, 0, 0, 1);
+insert into draft.null_ea values (111, 0, 1, 222, 0);
+insert into draft.null_ea values (111, 0, 0, 0, 1);
 
 
-select * from draft.stage where key_id in (select key_id from draft.keys);
+select *, dt_create from draft.stage where key_id in (select key_id from draft.keys);
 select * from draft.keys;
 
-select *
-from draft.keys
-where key_id = 5940436581358238122
+
+select
+    tup.2 as ch
+    , groupArrayIf((tup.10, del), tup.1 = 'bh') as bh
+    , groupArrayIf((tup.12, del), tup.1 = 'eh') as eh
+    , groupArrayIf((tup.8, del), tup.1 = 'ci') as art
+    , groupArrayIf((tup.11, del), tup.1 = 'bi') as bi
+from
+(
+    select
+        argMaxIf(tuple(* except (key_id)), dt_create, is_del = 0) as tup
+        , argMax(is_del, dt_create) as del
+    from draft.stage
+    where key_id in
+    (
+        select distinct arrayJoin([key_id, related_key_id])
+        from draft.keys where key_id in
+        (
+            select distinct arrayJoin([key_id, related_key_id])
+            from draft.keys where key_id in
+            (
+                select distinct arrayJoin([key_id, related_key_id])
+                from draft.keys where key_id in
+                (
+                    select distinct arrayJoin([key_id, related_key_id])
+                    from draft.keys where key_id in
+                    (
+                        select distinct arrayJoin([key_id, related_key_id])
+                        from draft.keys
+                        where key_id = 8178984314414479468
+                    )
+                )
+            )
+        )
+    )
+    group by key_id
+    having tup.1 <> ''
+    order by tup.1, del
+) group by tup.2;
+
+select * from draft.stage where key_id = 1871875760937111286
 
 
 
